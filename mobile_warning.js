dd609629c9b62ee832b29b7d35cc7d06ed153615
ElectricSquid <beastmode25800@gmail.com>
2016-07-29 16:37:42 -0500

    Removed function

--- mobile_warning.js
+++ mobile_warning.js
@@ -1,7 +1,3 @@
-function getDevice() {
-	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent) {
-		window.alert("We have detected you are using a mobile device. Please consider finding a computer as this site is not intended for use on mobile.");
-	}
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent) {
+	window.alert("We have detected you are using a mobile device. Please consider finding a computer as this site is not intended for use on mobile.");
 }
-
-window.onload = getDevice();
